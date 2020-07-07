cbab865dfd2d4bf8c924cd093a2cec09dcc138a4
権藤　俊 <gondo.syun0711@gmail.com>
2020-07-07 23:36:04 +0900

    chapter5の完成

---
+++ chapter5/Assets/DestroyObj.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObj : MonoBehaviour {
+
+    public float deleteTime = 2.0f;
+
+    // Start is called before the first frame update
+    void Start() {
+        Destroy(gameObject, deleteTime);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}

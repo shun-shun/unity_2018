ebc184ced49747670ca5f109c740a72bda0176cf
権藤　俊 <gondo.syun0711@gmail.com>
2020-07-08 23:02:19 +0900

    chapter6の完成(BGMはアセットがないため未実装)

---
+++ chapter6/Assets/Architecture textures pack/Roof tiles weathered/Out.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Out : MonoBehaviour {
+    void OnTriggerEnter(Collider col) {
+        if(col.gameObject.tag == "Player") {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
